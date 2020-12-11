7c4c47b1d9aab34ac656c272f09ae4029810d820
Jaeweon Kim <heyjaekim@gmail.com>
2020-12-11 13:41:36 +0900

    Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.0;
+
+contract TodoList{
+    uint public taskCount = 0; // provide the function to read the taskCount, and Sol. will give the function to read this keyword
+}
+
